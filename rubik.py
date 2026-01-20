b75adf597494e68178a39007f15a12eb67cdcb65
GORDIAN12 <davidgordian667@gmail.com>
2026-01-20 01:25:54 -0600

    update functions file rubik

--- rubik.py
+++ rubik.py
@@ -1,3 +1,5 @@
+import random
+
 from pyray import *
 import numpy as np
 
@@ -20,9 +22,55 @@ class Cube:
         self.model=load_model_from_mesh(self.mesh)
         self.model.transform=matrix_translate(self.center[0],self.center[1],self.center[2])
 
+    def rotate(self, axis, theta):
+        #crear la rotacion de matriz basada en ejes especificos
+        if axis==0:
+            rotation_matrix=np.array([
+                [1,0,0 ],
+                [0,np.cos(theta), -np.sin(theta)],
+                [0,np.sin(theta), np.cos(theta)]
+            ])
+        elif axis==1:
+            rotation_matrix=np.array([
+                [np.cos(theta),0, np.sin(theta)],
+                [0,1,0 ],
+                [-np.sin(theta),0, np.cos(theta)]
+            ])
+        elif axis==3:
+            rotation_matrix=np.array([
+                [np.cos(theta), -np.sin(theta),0],
+                [np.sin(theta), np.cos(theta),0],
+                [0, 0, 1]
+            ])
+        else:
+            raise ValueError("INVALID AXIS. USE 'x', 'y' or 'z'")
+
+        #aplicar la rotacion a la matriz y actualizar posicion
+        self.center=rotation_matrix @ self.center
+        #actualizar  la orientacion
+        self.orientation=rotation_matrix @ self.orientation
+
+    def get_rotation_axis_angle(self):
+        angle=np.arccos((np.trace(self.orientation)-1)/2)
+        if angle==0:
+            axis=np.array([0,0,0])
+        else:
+            rx=self.orientation[2,1] - self.orientation[1,2]
+            ry=self.orientation[0,2] - self.orientation[2,0]
+            rz=self.orientation[0,1] - self.orientation[1,0]
+            axis=np.array([rx,ry,rz])/(2*np.sin(angle))
+        axis=Vector3(axis[0],axis[1],axis[2])
+        return axis, np.degrees(angle)
+
 class Rubik:
     def __init__(self)->None:
         self.cubes=[]
+        self.is_rotating=False
+        self.is_rotation_angle=0
+        self.rotation_axis=None
+        self.level=None
+        self.segment=None
+        self.target_rotation=0
         self.generate_rubik(2)
 
     def generate_rubik(self,size):
@@ -73,5 +121,68 @@ class Rubik:
                     self.cubes.append([center,front,back,right,left,top,bottom])
 
         return self.cubes
-                    
+
+    def choose_piece(self, piece, axis_index, level):
+        if level == 0 and round(piece[0].center[axis_index],1)<0:
+            return True
+        elif level == 1 and round(piece[0].center[axis_index],1)==0:
+            return True
+        elif level == 2 and round(piece[0].center[axis_index],1)>0:
+            return True
+        return False
+
+    def get_face(self, axis, level):
+        axis_index=np.nonzero(axis)[0][0]
+        segment=[i for i, cube in enumerate(self.cubes)\
+                 if self.choose_piece(cube, axis_index, level)]
+        return segment
+
+    def handle_rotation(self, rotation_queue, animation_step=None):
+        if rotation_queue and not self.is_rotating:
+            self.target_rotation, self.rotation_axis, self.level=rotation_queue.pop(0)
+
+            if self.target_rotation>0:
+                self.target_rotation+=random.uniform(0,1)*10**-3
+            else:
+                self.target_rotation-=random.uniform(0,1)*10**-3
+            self.segment=self.get_face(self.rotation_axis, self.level)
+            self.rotation_angle=0
+            self.is_rotating=True
+
+        if self.is_routing:
+            if(self.rotation_angle != self.target_rotation):
+                diff=abs(self.target_rotation-self.rotation_angle)
+                delta_angle=min(np.radians(1), diff)
+
+                #incrementar la rotacion del angulo en la direccion correcta
+                self.rotation_angle+= delta_angle if self.target_rotation>0\
+                                                    else -delta_angle
+            else:
+                delta_angle=0
+
+                #para la rotacion cuando el objetivo de rotacion se alcanzo
+                self.is_rotating=False
+                if animation_step is not None:
+                    animation_step+=1
+            for id, cube in enumerate(self.cubes):
+                axis_index=np.nonzero(self.rotation_axis)[0][0]
+
+                if id in self.segment:
+                    for part_id in enumerate(cube):
+                        if self.target_rotation>0:
+                            self.cubes[id][part_id].rotate(axis_index,delta_angle)
+                        else:
+                            self.cubes[id][part_id].rotate(axis_index,-delta_angle)
+
+                        pos_x,pos_y,pos_z =self.cubes[id][part_id].center
+                        translation=matrix_translate(pos_x,pos_y,pos_z)
+                        rota,angle=self.cubes[id][part_id].get_rotation_axis_angle()
+                        rotation=matrix_rotate(rota,np.radians(angle))
+                        transform=matrix_multiply(rotation, translation)
+                        self.cubes[id][part_id].model.transform=transform
+        else:
+            self.is_rotating=True
+
+        return rotation_queue, animation_step
+
 
