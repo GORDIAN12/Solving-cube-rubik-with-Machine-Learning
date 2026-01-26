56e58f05807545390128dbb1489240852cb2d643
GORDIAN12 <davidgordian667@gmail.com>
2026-01-26 17:03:38 -0600

    file tester model ML

---
+++ ML/tester.py
@@ -0,0 +1,45 @@
+from ML.train_model import train_heuristic
+from scramble import move_scram
+from ML.gen_data import generate_dataset  # ajusta al nombre real
+import torch
+from ML.ida_star import ida_star
+
+# 1) lista fija de acciones compatibles con tu parser
+all_moves = [
+    "U","U'","U2",
+    "D","D'","D2",
+    "L","L'","L2",
+    "R","R'","R2",
+    "F","F'","F2",
+    "B","B'","B2"
+]
+
+N = 10
+X, y = generate_dataset(all_moves, N=N, samples=100000, seed=0)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("== INICIO ==")
+
+# entrenamiento
+model = train_heuristic(X, y, N=N, epochs=8, device=device)
+print("== ENTRENAMIENTO TERMINADO ==")
+
+# scramble
+scramble = move_scram.generate_movs(3)
+if isinstance(scramble, list):
+    scramble = " ".join(scramble)
+
+print("scramble:", scramble)
+
+state = move_scram.apply_scramble(
+    move_scram.SOLVED_STATE.copy(),
+    scramble,
+    move_scram.PERM
+)
+print("estado listo")
+
+print("== INICIANDO IDA* ==")
+sol = ida_star(state, all_moves, model, device=device)
+
+print("== IDA* TERMINÓ ==")
+print("sol:", sol)
