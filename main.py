6b8218508aa159486d4944c3df25e7a374d578de
GORDIAN12 <davidgordian667@gmail.com>
2026-01-28 22:35:32 -0600

    update main

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("this the main of the project")
+from scramble import move_scram
+
+x=input("selecciona la opcion a ejecutar")
+print("1. simular cubo rubik con scramble simple")
+print("2. simular cubo rubik con scramble con solucion inversa")
+print("3. simular cubo rubik con modelo de entrenamiento")
+
+
+
+
